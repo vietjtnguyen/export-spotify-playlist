7681d6d959166f1d782430e4a447fd44a36844aa
vietjtnguyen <viet@vietjtnguyen.com>
2013-05-16 09:41:17 -0700

    Added argument support. Handled 404 errors.

--- export-spotify-playlist.py
+++ export-spotify-playlist.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
+#!/usr/bin/python
 
-# <codecell>
-
+import argparse
 import csv
 import collections
 import re
+import sys
+import urllib2
 
 # http://stackoverflow.com/questions/2087370/decode-html-entities-in-python-string
 import HTMLParser
@@ -14,56 +14,62 @@ html_parser = HTMLParser.HTMLParser()
 # http://wwwsearch.sourceforge.net/mechanize/
 import mechanize
 
-# <codecell>
+# http://docs.python.org/2.7/library/argparse.html
+parser = argparse.ArgumentParser(description='Exports a Spotify playlist to stdout or csv.')
+parser.add_argument('playlist', nargs='+', help='a Spotify playlist URL or URI')
+parser.add_argument('--csv', '-c', dest='export_csv', action='store_true', help='exports playlist to a CSV file')
+arguments = parser.parse_args()
 
-arg1 = 'http://open.spotify.com/user/vietjtnguyen/playlist/58BEX3K4yuhqlkF1lAn0r5'
-arg1 = 'spotify:user:vietjtnguyen:playlist:58BEX3K4yuhqlkF1lAn0r5'
+for playlist in arguments.playlist:
 
-if arg1.startswith('http'):
-    playlist_url = arg1
-else:
-    playlist_url = 'http://open.spotify.com/user/{}/playlist/{}'.format(*re.search('spotify:user:(.+):playlist:(.+)', arg1).groups())
+	if playlist.startswith('http'):
+		playlist_url = playlist
+	else:
+		playlist_url = 'http://open.spotify.com/user/{}/playlist/{}'.format(*re.search('spotify:user:(.+):playlist:(.+)', playlist).groups())
 
-print(playlist_url)
+	print('')
+	print(playlist_url)
 
-# <codecell>
+	playlist_br = mechanize.Browser()
+	playlist_br.open(playlist_url)
 
-playlist_br = mechanize.Browser()
-playlist_br.open(playlist_url)
+	playlist_name = re.search(r'<h1 itemprop="name">(.+)</h1>', playlist_br.response().read()).groups()[0]
+	print(playlist_name)
+	print('')
 
-# <codecell>
+	# http://docs.python.org/2/library/functions.html#filter
+	track_links = filter(lambda x: x.url.startswith('/track/'), playlist_br.links())
 
-# http://docs.python.org/2/library/functions.html#filter
-track_links = filter(lambda x: x.url.startswith('/track/'), playlist_br.links())
+	# http://docs.python.org/2/library/collections.html#ordereddict-objects
+	tracks = collections.OrderedDict()
+	for track_link in track_links:
+		if not tracks.has_key(track_link.url):
+			tracks[track_link.url] = {'mechanized_link': track_link, 'track_url': 'http://open.spotify.com' + track_link.url, 'title': track_link.text}
 
-# <codecell>
+	for track in tracks.values():
+		try:
+			track_html = mechanize.urlopen(track['track_url']).read()
+		except urllib2.HTTPError as e:
+			print('{:}, HTTP Error 404, {:} not found'.format(track['title'], track['track_url']))
+			track['artist_url'], track['artist'], track['album_url'], track['album'] = ('', '', '', '')
+			continue
 
-# http://docs.python.org/2/library/collections.html#ordereddict-objects
-tracks = collections.OrderedDict()
-for track_link in track_links:
-    tracks[track_link.url] = {'mechanized_link': track_link, 'track_url': 'http://open.spotify.com' + track_link.url}
+		# http://docs.python.org/2/library/re.html
+		matches = re.search(r'<h1 itemprop="name">(.+)</h1>[ \t\n\r]*<h2> by <a href="(/artist/.+)">(.+)</a></h2>[ \t\n\r]*</div>[ \t\n\r]*<h3>Tracks in <a href="(/album/.+)">(.+)</a></h3>', track_html)
+		track['title'], track['artist_url'], track['artist'], track['album_url'], track['album'] = matches.groups()
+		track['artist_url'] = 'http://open.spotify.com' + track['artist_url']
+		track['album_url'] = 'http://open.spotify.com' + track['album_url']
+		track['title'], track['artist'], track['album'] = map(html_parser.unescape, (track['title'], track['artist'], track['album']))
+		print('{artist} - {album} - {title}'.format(**track))
 
-# <codecell>
-
-for track in tracks.values():
-    track_html = mechanize.urlopen(track['track_url']).read()
-    # http://docs.python.org/2/library/re.html
-    matches = re.search(r'<h1 itemprop="name">(.+)</h1>[ \t\n\r]*<h2> by <a href="(/artist/.+)">(.+)</a></h2>[ \t\n\r]*</div>[ \t\n\r]*<h3>Tracks in <a href="(/album/.+)">(.+)</a></h3>', track_html)
-    track['title'], track['artist_url'], track['artist'], track['album_url'], track['album'] = matches.groups()
-    track['artist_url'] = 'http://open.spotify.com' + track['artist_url']
-    track['album_url'] = 'http://open.spotify.com' + track['album_url']
-    track['title'], track['artist'], track['album'] = map(html_parser.unescape, (track['title'], track['artist'], track['album']))
-    print(track)
-
-# <codecell>
-
-# http://docs.python.org/2/library/csv.html
-with open('out.csv', 'wb') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Artist', 'Album', 'Title', 'Artist URL', 'Album URL', 'Track URL'])
-    for track in tracks.values():
-        csv_writer.writerow([track[key] for key in ['artist', 'album', 'title', 'artist_url', 'album_url', 'track_url']])
-
-# <codecell>
-
+	if arguments.export_csv:
+		# http://docs.python.org/2/library/csv.html
+		csv_file_name = '{}.csv'.format(playlist_name)
+		with open(csv_file_name, 'wb') as csv_file:
+			csv_writer = csv.writer(csv_file)
+			csv_writer.writerow(['Artist', 'Album', 'Title', 'Artist URL', 'Album URL', 'Track URL'])
+			for track in tracks.values():
+				csv_writer.writerow([track[key] for key in ['artist', 'album', 'title', 'artist_url', 'album_url', 'track_url']])
+		print('')
+		print('Playlist written to "{:}"'.format(csv_file_name))
 
